7d1c8c4b837389d0553498572b8a2e2c3c3591b6
artwel <welsch.arthur@gmail.com>
2020-02-12 12:51:13 +0100

    Initial commit

---
+++ src/main/java/de/artwel/wow/ClassicItemsApplication.java
@@ -0,0 +1,13 @@
+package de.artwel.wow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/artwel/wow/ClassicItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.artwel.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
